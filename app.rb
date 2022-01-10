15b068cf652399b8c54af13652463c541e8fc876
Marty Naselli <marty@swiftbunny.com>
2022-01-10 15:44:45 -0500

    echo gifs from giphy

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
 require 'dotenv'
+require 'json'
+require 'net/http'
 
 Dotenv.load
 
 get '/' do
-	"Hello world!!"
+  send_file 'index.html'
+end
+
+get '/get_gifs' do
+  uri = URI "https://api.giphy.com/v1/gifs/search?" +
+    URI.encode_www_form({
+      q: "no stop",
+      rating: "r",
+      api_key: ENV['GIPHY_API_KEY']
+    })
+
+  response = Net::HTTP.get(uri)
+  response_obj = JSON.parse(response)
+
+  original_gif_urls = response_obj['data'].collect {|x| x['images']['original']['url']}
+
+  { urls: original_gif_urls }.to_json
 end
